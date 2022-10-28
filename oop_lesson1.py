43125fa719be14c980423c530440d8f03b5abce8
Neme Nwaka <ccnwaka1988@gmail.com>
2022-10-28 12:24:54 +0200

    updated person class and customer class

--- oop_lesson1.py
+++ oop_lesson1.py
@@ -12,3 +12,27 @@ class Bank :
 
 x = Bank(45367, "Savvy", "Burgundy")
 print(x)
+class Person :
+    def __init__(self, id, name, address, phone_number):
+        self.id = id
+        self.name = name
+        self.address = address
+        self.phone_number = phone_number
+class Customer (Person):
+    def __init__(self, id, name, address, phone_number,account_number):
+        super().__init__(id, name, address, phone_number)
+        self.account_number =account_number
+    def general_inquiry():
+        pass
+    def deposit_money():
+        pass
+    def withdraw_money():
+        pass
+    def open_account():
+        pass
+    def close_account():
+        pass
+    def apply_loan():
+        pass
+    def request_card():
+        pass
